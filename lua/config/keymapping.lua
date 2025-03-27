6a4e5833f92effb9b809994f1fc967217d4676d1
Dmitrii Orlik <drlkmt@yandex.ru>
2025-03-27 09:53:51 +0300

    Clean lazy.nvim plugin.

---
+++ lua/config/keymapping.lua
@@ -0,0 +1,11 @@
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+
+
+-- Neo-Tree plugin keybindings
+vim.keymap.set('n', '<Leader>e', ':Neotree position=float<CR>', {
+    noremap = true,  -- Отключает рекурсивные маппинги
+    silent = true,   -- Подавляет вывод команды в командную строку
+  })
