72a9dbcea5e8ec62d761e192bea1fc3cafbb0196
Dmitrii Orlik <drlkmt@yandex.ru>
2025-06-08 11:43:19 +0300

    LazyVim any change

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,7 +20,7 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- use mini.starter instead of alpha
-    -- { import = "lazyvim.plugins.extras.ui.mini-starter" },
+    -- { import = "lazyvim.plugins.extras" },
 
     -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
     -- { import = "lazyvim.plugins.extras.lang.json" },
