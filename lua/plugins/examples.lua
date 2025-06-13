e4ef85dbdcfc595ea45f9ab917abcea347c78903
Dmitrii Orlik <drlkmt@yandex.ru>
2025-06-13 20:18:50 +0300

    Remove rust plugin

--- lua/plugins/examples.lua
+++ lua/plugins/examples.lua
@@ -215,10 +215,4 @@ return {
       { "<leader>e", false },
     },
   },
-
-  {
-    "mrcjkb/rustaceanvim",
-    version = "^6", -- Recommended
-    lazy = false, -- This plugin is already lazy
-  },
 }
